dd56fe693131a4068a2d3600c26e34ca37d25129
Celimpilo36 <mayisa3626@gmail.com>
2025-09-24 09:44:39 -0400

    adding example code for DIP

--- Introduction/DIP_EXAMPLE.py
+++ Introduction/DIP_EXAMPLE.py
@@ -1,28 +1,31 @@
 from abc import ABC, abstractmethod
 
-#abstraction
-class Worker:
+
+# Abstraction
+class Worker(ABC):
     @abstractmethod
-    def work(self):
+    def work(self) -> None:
         pass
 
-#Low level modules
+
+# Low level modules
 class Developer(Worker):
-    def work(self):
+    def work(self) -> None:
         print("Developer is writing code")
 
+
 class Designer(Worker):
-    def work(self): 
+    def work(self) -> None:
         print("Designer is designing fireWorks")
 
-#high level modules
+
+# high level modules
 class Mananger:
-    def __init__(self,worker: Worker):
+    def __init__(self, worker: Worker):
         self.worker = worker
 
     def manange(self):
         self.worker.work()
-
 
 
 dev = Developer()
