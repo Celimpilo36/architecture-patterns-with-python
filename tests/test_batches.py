21b37b3e6c2e1191c03fba2fd4fe948f05c45e0f
Celimpilo36 <mayisa3626@gmail.com>
2026-04-18 13:52:31 +0200

    initial project structure setup

---
+++ tests/test_batches.py
@@ -0,0 +1,10 @@
+from domain.model import Batch, OrderLine
+from datetime import date
+
+def test_allocating_to_a_batch_reduces_the_aailable_quantity() -> None:
+    batch = Batch("batch-001","SMALL-TABLE", qty=20, eta=date.today())
+    line = OrderLine('order-ref', "SMALL-TABLE",2)
+
+    batch.allocate(line)
+
+    assert batch.available_quantity == 18
