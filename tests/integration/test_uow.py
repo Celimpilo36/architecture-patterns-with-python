cecf3aca67df51b318c5bb2696f5c74d4ebea1c0
Celimpilo36 <mayisa3626@gmail.com>
2026-08-10 09:24:57 +0200

    Explicit Test for commit/rollback Behavior

--- tests/integration/test_uow.py
+++ tests/integration/test_uow.py
@@ -2,6 +2,7 @@ import unittest
 from src.service_layer import unit_of_work
 from tests.conftest import insert_batch, get_allocated_batch_ref, get_session_factory
 from src.domain import model
+from sqlalchemy import text
 
 
 class TestUOW(unittest.TestCase):
@@ -15,9 +16,6 @@ class TestUOW(unittest.TestCase):
         self.session = self.session_factory()
         return super().setUp()
     
-    def tearDown(self) -> None:
-        self.session.close()
-        return super().tearDown()
 
     def test_uow_retrieve_a_batch_and_allocate_to_it(self):
         insert_batch(self.session, 'batch1', 'HIPSTER-WORKBENCH',100, None)
@@ -32,3 +30,27 @@ class TestUOW(unittest.TestCase):
 
         batchref = get_allocated_batch_ref(self.session, '01', 'HIPSTER-WORKBENCH')
         self.assertEqual(batchref, 'batch1')
+
+    def test_rolls_back_uncommitted_work_by_defaulf(self):
+        uow = unit_of_work.SqlAlchemyUnitOfWork(get_session_factory())
+
+        with uow:
+            insert_batch(uow.session, 'batch1', 'MEDIUM-PLINTH', 100, None)
+
+        new_session = get_session_factory()
+        rows = list(new_session().execute(text('SELECT * FROM "bathces"')))
+        self.assertTrue(rows, [])
+
+
+    def test_rolls_back_on_error(self):
+        class MyException(Exception):
+            pass
+        uow = unit_of_work.SqlAlchemyUnitOfWork(get_session_factory())
+        with self.assertRaises(MyException):
+            with uow:
+                insert_batch(uow.session, 'batch1', 'LARGE-FORK', 100, None)
+                self.assertRaises(MyException)
+
+        new_session = get_session_factory()
+        rows = list(new_session().execute(text('SELECT * FROM "batches"')))
+        self.assertEqual(rows, [])
